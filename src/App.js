1fb517056588e510e5406dfce5542eb6cacfc6c8
szymonjakob <szymon@studiosj.pl>
2023-11-21 20:57:42 +0100

    Added styling accents to opened pane

--- src/App.js
+++ src/App.js
@@ -42,12 +42,10 @@ function AccordionItem({num, title, text}) {
   }
 
   return (
-    <div className="item">
-      <div className="header" onClick={handleToggle}>
-        <p className="number">{num < 9 ? `0${num}` : num}</p>
-        <p className="title">{title}</p>
-        <p className="icon">{ isOpen ? '-' : '+'}</p>
-      </div>
+    <div className={`item ${isOpen ? 'open' : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num}` : num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{ isOpen ? '-' : '+'}</p>
       {isOpen && <div className="content-box">{text}</div>}
   </div>
   );
